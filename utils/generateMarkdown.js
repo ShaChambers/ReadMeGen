42bff54a442c332fd2266a07d4140f10317a6413
Shaka Suzy <shaheedchambers@gmail.com>
2023-01-09 11:38:31 -0500

    actually ran it and recorded use

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,16 +10,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== "None") {
-    return `This application is covered under the ${license} license.`;
-  } else {
-    return `N/A`;
-  }
-}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
@@ -46,7 +36,6 @@ If your README is long, add a table of contents to make it easy for users to fin
 - [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
-${renderLicenseLink(data.license)}
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
